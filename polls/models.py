2f63f45f1597cdd396d84e556e5426f9871982aa
Duwj <simon334@126.com>
2016-03-21 00:42:58 +0800

    test

---
+++ polls/models.py
@@ -0,0 +1,27 @@
+from __future__ import unicode_literals
+import datetime
+from django.db import models
+from django.utils import timezone
+
+# Create your models here.
+
+class Question(models.Model):
+    question_text=models.CharField(max_length=200)
+    pub_date=models.DateTimeField('date published')
+    #python3 =  __str__
+    def __unicode__(self):
+           return self.question_text
+    #user define
+    def was_published_recently(self):
+           return self.pub_date >= timezone.now() - datetime.timedelta(days=1)
+           was_published_recently.admin_order_field='pub_date'
+           was_published_recently.boolean=True
+           was_published_recently.short_desciption="Published recently"
+
+class Choice(models.Model):
+    question=models.ForeignKey(Question,on_delete=models.CASCADE)
+    choice_text=models.CharField(max_length=200)
+    votes=models.IntegerField(default=0)
+        #python3 =  __str_
+    def __unicode__(self):
+           return self.choice_text
